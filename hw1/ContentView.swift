41525a1f22bafeadad2313944100321bb1cb1dab
sushma1261 <39758057+sushma1261@users.noreply.github.com>
2024-10-05 12:20:38 -0700

    Initial Commit

---
+++ hw1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hw1
+//
+//  Created by Sushma Manthena on 05/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hw1/hw1App.swift
@@ -0,0 +1,17 @@
+//
+//  hw1App.swift
+//  hw1
+//
+//  Created by Sushma Manthena on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct hw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
